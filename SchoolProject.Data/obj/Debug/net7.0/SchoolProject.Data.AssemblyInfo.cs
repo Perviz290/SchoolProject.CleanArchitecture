93387e3be9c9685161ae9c7d04e338ca267ad72f
Perviz290 <veliyevperviz290@gmail.com>
2024-01-31 10:40:31 +0400

    int

--- SchoolProject.Data/obj/Debug/net7.0/SchoolProject.Data.AssemblyInfo.cs
+++ SchoolProject.Data/obj/Debug/net7.0/SchoolProject.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolProject.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d74c22aea010b746517d1cd87952fca64d065c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolProject.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolProject.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SchoolProject.Service/obj/Debug/net7.0/SchoolProject.Service.AssemblyInfo.cs
+++ SchoolProject.Service/obj/Debug/net7.0/SchoolProject.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolProject.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b6bb38980b4eef570b87f552382e1aa8731f36c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5d74c22aea010b746517d1cd87952fca64d065c")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolProject.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolProject.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
